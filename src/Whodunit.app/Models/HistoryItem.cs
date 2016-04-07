f2f378cf6af02446c9362fefca0ab85bf43ba2b0
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-04-06 18:10:46 -0700

    Renamed table.

--- src/Whodunit.app/Models/HistoryItem.cs
+++ src/Whodunit.app/Models/HistoryItem.cs
@@ -8,6 +8,7 @@
     [PrimaryKey(HistoryItem.PrimaryKeyName, autoIncrement = true)]
     public class HistoryItem {
 
+        //TODO: Why was it this?: "[whodunit].[dbo].[Whodunit.HistoryItem]";
         public const string TableName = "[whodunit].[dbo].[Whodunit.HistoryItem]";
         public const string PrimaryKeyName = "AutoId";
 
