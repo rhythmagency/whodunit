2a1f96b7ca95a3a4ee956785f26c1cd37be7347d
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-04-06 18:13:26 -0700

    Simplified code.

--- src/Whodunit.app/Models/HistoryItem.cs
+++ src/Whodunit.app/Models/HistoryItem.cs
@@ -4,8 +4,8 @@
     using Umbraco.Core.Persistence;
     using Umbraco.Core.Persistence.DatabaseAnnotations;
 
-    [TableName(HistoryItem.TableName)]
-    [PrimaryKey(HistoryItem.PrimaryKeyName, autoIncrement = true)]
+    [TableName(TableName)]
+    [PrimaryKey(PrimaryKeyName, autoIncrement = true)]
     public class HistoryItem {
 
         public const string TableName = "[Whodunit.HistoryItem]";
