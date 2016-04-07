1f5cb15b368632961c935a898e16866faf228b21
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-04-06 17:34:46 -0700

    Added copying to Grunt task.

--- src/gruntfile.js
+++ src/gruntfile.js
@@ -1,8 +1,46 @@
 module.exports = function(grunt) {
 
+    // Variables.
+    var appProject = "Whodunit.app";
+    var website = "Whodunit.test";
+    var binaries = ["Whodunit.app.dll", "Whodunit.app.pdb"];
+    var temp = "./WhodunitTemp/package/";
+
     // Initialize Grunt tasks.
     grunt.initConfig({
         "pkg": grunt.file.readJSON('package.json'),
+        copy: {
+            // Main is used to copy files to the sample website.
+            main: {
+                files: [
+                    {
+                        // Frontend files.
+                        expand: true,
+                        src: ["App_Plugins/**"],
+                        dest: website + "/",
+                        cwd: appProject + "/"
+                    }
+                ]
+            },
+            // Package is used to copy files to create the Umbraco package.
+            package: {
+                files: [
+                    {
+                        // Frontend files.
+                        expand: true,
+                        src: ["App_Plugins/**"],
+                        dest: temp,
+                        cwd: appProject + "/"
+                    }, {
+                        // App binaries.
+                        expand: true,
+                        src: binaries,
+                        dest: temp + "bin/",
+                        cwd: appProject + "/bin/Release/"
+                    }
+                ]
+            }
+        },
         umbracoPackage: {
             main: {
                 src: "./files",
@@ -23,11 +61,15 @@ module.exports = function(grunt) {
     });
 
     // Load NPM tasks.
+    grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-umbraco-package");
 
     // Register Grunt tasks.
     grunt.registerTask("default",
-        // The "default" task is for general development of ReadingAge.
-        [ "umbracoPackage:main" ]);
+        // The "default" task is for general development of Whodunit.
+        [ "copy:main" ]);
+    grunt.registerTask("package",
+        // The "package" task will create an Umbraco package of Whodunit.
+        [ "copy:package" ]);
 
 };
