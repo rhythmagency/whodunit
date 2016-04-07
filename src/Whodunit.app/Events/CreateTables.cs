667de152b465e6de28615a2a1d73c6f379eb78a0
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-04-06 18:11:48 -0700

    Removed some unnecessary using statements.

--- src/Whodunit.app/Events/CreateTables.cs
+++ src/Whodunit.app/Events/CreateTables.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Umbraco.Core;
 using Umbraco.Core.Logging;
 using Umbraco.Core.Persistence;
 using Whodunit.app.Models;
 
-namespace Whodunit.app.Events {
+namespace Whodunit.app.Events
+{
     public class CreateTables : ApplicationEventHandler {
 
         protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext) {
