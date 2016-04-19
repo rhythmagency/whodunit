1aa85246695d4fc6d62571ed0f359ed60505a2a8
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-04-19 16:31:36 -0700

    Oops, reversed the subtraction operands.

--- src/Whodunit.app/HistoryHelper.cs
+++ src/Whodunit.app/HistoryHelper.cs
@@ -74,7 +74,7 @@
             // Split message into parts to avoid SQL errors.
             for (var i = 0; i < message.Length; i += 3000)
             {
-                var length = Math.Min(3000, i - message.Length);
+                var length = Math.Min(3000, message.Length - i);
                 var part = message.Substring(i, length);
                 if (parts.Count > 0)
                 {
